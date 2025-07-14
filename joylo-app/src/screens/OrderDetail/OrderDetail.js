b2d9f900071708f2b7c6554ed198311337592288
Hasnat Amin <hasnatamin065@gmail.com>
2025-07-14 17:07:48 +0500

    Update OrderDetail.js

--- joylo-app/src/screens/OrderDetail/OrderDetail.js
+++ joylo-app/src/screens/OrderDetail/OrderDetail.js
@@ -279,7 +279,7 @@ function OrderDetail(props) {
       <View style={styles().bottomContainer(currentTheme)}>
         <PriceRow theme={currentTheme} title={getTranslation('total')} currency={configuration.currencySymbol} price={total.toFixed(2)} />
         {
-          ORDER_STATUS_ENUM.DELIVERED &&
+          order?.orderStatus === ORDER_STATUS_ENUM.DELIVERED &&
           <View style={{ marginTop: scale(20), marginRight: scale(20), marginLeft: scale(20), marginBottom: scale(3) }}>
             <Button text={getTranslation('rate_your_rider')} buttonProps={{ onPress: handleRatingYourRiderBtn }} buttonStyles={styles().ratingButtonContainer(currentTheme)} textProps={{ textColor: currentTheme.black }} textStyles={{ ...alignment.Pmedium }}></Button>
           </View>
