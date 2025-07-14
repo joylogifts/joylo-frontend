5d6459aa34f4931619c4b01bbbf636c8e7465d5b
Umer F <uf.umerfarooq67@gmail.com>
2025-07-14 18:37:41 +0500

    integrated translaiton in admin category, option - v2

--- joylo-admin/lib/ui/screen-components/protected/super-admin/add-on/view/header/screen-header/index.tsx
+++ joylo-admin/lib/ui/screen-components/protected/super-admin/add-on/view/header/screen-header/index.tsx
@@ -1,6 +1,7 @@
 // Interface and Types
 
 // Components
+import { useLangTranslation } from '@/lib/context/global/language.context';
 import HeaderText from '@/lib/ui/useable-components/header-text';
 import TextIconClickable from '@/lib/ui/useable-components/text-icon-clickable';
 
@@ -12,6 +13,8 @@ import { useTranslations } from 'next-intl';
 const AddonHeader = ({ setIsAddAddonVisible }: IAddonHeaderProps) => {
   // Hooks
   const t = useTranslations();
+  const { getTranslation, selectedLanguage } = useLangTranslation();
+
   return (
     <div className="sticky top-0 z-10 w-full flex-shrink-0 bg-white p-3 shadow-sm">
       <div className="flex w-full justify-between">
